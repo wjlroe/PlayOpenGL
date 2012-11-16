acdc89425fa43c00875b5ce42700e3313ba3d866
William Roe <willroe@gmail.com>
2012-11-16 01:35:18 +0000

    blank render engine header

---
+++ PlayOpenGL/IRenderingEngine.hpp
@@ -0,0 +1,14 @@
+//
+//  IRenderingEngine.hpp
+//  PlayOpenGL
+//
+//  Created by William Roe on 16/11/2012.
+//  Copyright (c) 2012 William Roe. All rights reserved.
+//
+
+#ifndef PlayOpenGL_IRenderingEngine_hpp
+#define PlayOpenGL_IRenderingEngine_hpp
+
+
+
+#endif
