7f351de0c7a8f58fe637cceae02b13615cff9c6a
William Roe <willroe@gmail.com>
2013-09-04 18:44:31 +0100

    whitespace

--- PlayOpenGL/RenderingEngine1.cpp
+++ PlayOpenGL/RenderingEngine1.cpp
@@ -53,12 +53,12 @@ void RenderingEngine1::Initialize(int width, int height)
     const float coneRadius = 0.5f;
     const float coneHeight = 1.866f;
     const int coneSlices = 40;
-    
+
     // generate vertices here
     {
-        
+
         m_cone.resize((coneSlices + 1) * 2);
-        
+
         // Initialize the vertices of the triangle strip
         vector<Vertex>::iterator vertex = m_cone.begin();
         const float dtheta = TwoPi / coneSlices;
@@ -66,12 +66,12 @@ void RenderingEngine1::Initialize(int width, int height)
             //grayscale gradient
             float brightness = abs(sin(theta));
             vec4 color(brightness, brightness, brightness, 1);
-            
+
             // apex vertex
             vertex->Position = vec3(0, 1, 0);
             vertex->Color = color;
             vertex++;
-            
+
             // Rim vertex
             vertex->Position.x = coneRadius * cos(theta);
             vertex->Position.y = 1 - coneHeight;
@@ -80,11 +80,11 @@ void RenderingEngine1::Initialize(int width, int height)
             vertex++;
         }
     }
-    
+
     {
         // Allocate space for the disk vertices
         m_disk.resize(coneSlices + 2);
-        
+
         // Initialize the center vertex of the triangle fan
         vector<Vertex>::iterator vertex = m_disk.begin();
         vertex->Color = vec4(0.75, 0.75, 0.75, 1);
@@ -92,7 +92,7 @@ void RenderingEngine1::Initialize(int width, int height)
         vertex->Position.y = 1 - coneHeight;
         vertex->Position.z = 0;
         vertex++;
-        
+
         // Initialize the rim vertices of the triangle fan
         const float dtheta = TwoPi / coneSlices;
         for (float theta = 0; vertex != m_disk.end(); theta += dtheta) {
@@ -103,14 +103,14 @@ void RenderingEngine1::Initialize(int width, int height)
             vertex++;
         }
     }
-    
+
     glGenRenderbuffersOES(1, &m_depthRenderbuffer);
     glBindRenderbufferOES(GL_RENDERBUFFER_OES, m_depthRenderbuffer);
     glRenderbufferStorageOES(GL_RENDERBUFFER_OES,
                              GL_DEPTH_COMPONENT16_OES,
                              width,
                              height);
-    
+
     glGenFramebuffersOES(1, &m_framebuffer);
     glBindFramebufferOES(GL_FRAMEBUFFER_OES, m_framebuffer);
     glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES,
@@ -121,15 +121,15 @@ void RenderingEngine1::Initialize(int width, int height)
                                  GL_DEPTH_ATTACHMENT_OES,
                                  GL_RENDERBUFFER_OES,
                                  m_depthRenderbuffer);
-    
+
     glBindRenderbufferOES(GL_RENDERBUFFER_OES, m_colorRenderbuffer);
-    
+
     glViewport(0, 0, width, height);
     glEnable(GL_DEPTH_TEST);
-    
+
     glMatrixMode(GL_PROJECTION);
     glFrustumf(-1.6f, 1.6, -2.4, 2.4, 5, 10);
-    
+
     glMatrixMode(GL_MODELVIEW);
     glTranslatef(0, 0, -7);
 }
@@ -139,7 +139,7 @@ void RenderingEngine1::UpdateAnimation(float timeStep)
     if (m_animation.Current == m_animation.End) {
         return;
     }
-    
+
     m_animation.Elapsed += timeStep;
     if (m_animation.Elapsed >= AnimationDuration) {
         m_animation.Current = m_animation.End;
@@ -152,34 +152,34 @@ void RenderingEngine1::UpdateAnimation(float timeStep)
 void RenderingEngine1::OnRotate(DeviceOrientation orientation)
 {
     vec3 direction;
-    
+
     switch (orientation) {
         case DeviceOrientationUnknown:
         case DeviceOrientationPortrait:
             direction = vec3(0, 1, 0);
             break;
-            
+
         case DeviceOrientationPortraitUpsideDown:
             direction = vec3(0, -1, 0);
             break;
-            
+
         case DeviceOrientationFaceDown:
             direction = vec3(0, 0, -1);
             break;
-            
+
         case DeviceOrientationFaceUp:
             direction = vec3(0, 0, 1);
             break;
-            
+
         case DeviceOrientationLandscapeLeft:
             direction = vec3(+1, 0, 0);
             break;
-            
+
         case DeviceOrientationLandscapeRight:
             direction = vec3(-1, 0, 0);
             break;
     }
-    
+
     m_animation.Elapsed = 0;
     m_animation.Start = m_animation.Current = m_animation.End;
     m_animation.End = Quaternion::CreateFromVectors(vec3(0, 1, 0), direction);
@@ -190,25 +190,25 @@ void RenderingEngine1::Render() const
     glClearColor(0.5f, 0.5f, 0.5f, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glPushMatrix();
-    
+
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
-    
+
     mat4 rotation(m_animation.Current.ToMatrix());
     glMultMatrixf(rotation.Pointer());
-    
+
     // Draw the cone
     glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &m_cone[0].Position.x);
     glColorPointer(4, GL_FLOAT, sizeof(Vertex), &m_cone[0].Color.x);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, m_cone.size());
-    
+
     // Draw the disk that caps off the base of the cone
     glVertexPointer(3, GL_FLOAT, sizeof(Vertex), &m_disk[0].Position.x);
     glColorPointer(4, GL_FLOAT, sizeof(Vertex), &m_disk[0].Color.x);
     glDrawArrays(GL_TRIANGLE_FAN, 0, m_disk.size());
-    
+
     glDisableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_COLOR_ARRAY);
-    
+
     glPopMatrix();
 }
