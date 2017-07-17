841464f2cd03312d0d88876e502aaf99fb8a3025
William Roe <git@wjlr.org.uk>
2017-07-17 16:58:55 +0100

    WIP: GLLog

--- src/hello_glfw/hello.cpp
+++ src/hello_glfw/hello.cpp
@@ -1,9 +1,42 @@
+#include <stdarg.h>
+#include <time.h>
+#define GL_LOG_FILE "gl.log"
 #include <GL/glew.h>
 #define GLFW_DLL
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <strsafe.h>
 #include <windows.h>
+
+bool RestartGLLog() {
+  FILE *File = fopen(GL_LOG_FILE, "w");
+  if (!File) {
+    fprintf(stderr,
+            "ERROR: could not open GL_LOG_FILE log file %s for writing\n",
+            GL_LOG_FILE);
+    return false;
+  }
+  time_t Now = time(NULL);
+  char *Date = ctime(&Now);
+  fprintf(File, "GL_LOG_FILE log. local time %s\n", Date);
+  fclose(File);
+  return true;
+}
+
+bool GLLog(const char *Message, ...) {
+  va_list ArgPtr;
+  FILE *File = fopen(GL_LOG_FILE, "a");
+  if (!File) {
+    fprintf(stderr, "Error: could not open GL_LOG_FILE %s file for appending\n",
+            GL_LOG_FILE);
+    return false;
+  }
+  va_start(ArgPtr, Message);
+  vfprintf(File, Message, ArgPtr);
+  va_end(ArgPtr);
+  fclose(File);
+  return true;
+}
 
 #if 0
 void ErrorExit(const char *lpszFunction) {
