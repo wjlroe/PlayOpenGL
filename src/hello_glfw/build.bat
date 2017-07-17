abad2304f8e1e3438bcf83d8f89aa2172cf9e794
William Roe <git@wjlr.org.uk>
2017-07-17 16:57:57 +0100

    Format build times so emacs doesn't think they're line numbers

--- src/hello_glfw/build.bat
+++ src/hello_glfw/build.bat
@@ -2,9 +2,9 @@
 
 SET /A errno=0
 
-echo %time%
+echo time now: %time%
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-echo %time%
+echo time now: %time%
 
 set GLEW_INCLUDE_PATH="%USERPROFILE%\Downloads\glew-2.0.0-win32\glew-2.0.0\include"
 set GLEW_LIB_PATH="%USERPROFILE%\Downloads\glew-2.0.0-win32\glew-2.0.0\lib\Release\x64"
@@ -13,10 +13,10 @@ set GLFW_LIB_PATH="%USERPROFILE%\Downloads\glfw-3.2.1.bin.WIN64\glfw-3.2.1.bin.W
 
 IF NOT EXIST build mkdir build
 pushd build
-echo %time%
+echo time now: %time%
 cl -FC -Zc:strictStrings -Zi -EHsc ..\hello.cpp -I %GLEW_INCLUDE_PATH% -I %GLFW_INCLUDE_PATH% /link %GLEW_LIB_PATH%\glew32.lib %GLFW_LIB_PATH%\glfw3.lib opengl32.lib glu32.lib %GLFW_LIB_PATH%\glfw3dll.lib /SUBSYSTEM:CONSOLE
 IF %ERRORLEVEL% NEQ 0 SET /A errno=%ERRORLEVEL%
-echo %time%
+echo time now: %time%
 popd
 
 EXIT /B %errno%
