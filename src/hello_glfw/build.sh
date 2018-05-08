2b73c01ac84fa567d720f877463714daad4b8503
William Roe <git@wjlr.org.uk>
2018-05-08 17:50:51 +0100

    Initial build script for unixes

---
+++ src/hello_glfw/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" #> /dev/null
+mkdir -p ./build
+pushd ./build #> /dev/null
+c++ -std=c++11 -o hello.exe ../hello.cpp -I/usr/local/include -I/opt/X11/include -g `pkg-config --cflags glew glfw3` `pkg-config --libs glew glfw3`
