54ee9b9fd8d051ba5ead68fc8aea21b075fb8811
William Roe <git@wjlr.org.uk>
2018-05-13 20:34:42 +0100

    build.sh to work on macos as well as linux

--- src/hello_glfw/build-macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "$DIR" #> /dev/null
-mkdir -p ./build
-pushd ./build #> /dev/null
-c++ -std=c++11 -o hello_unix.exe ../hello_unix.cpp -I/usr/local/include -I/opt/X11/include -g `pkg-config --cflags glew glfw3` `pkg-config --libs glew glfw3`
--- src/hello_glfw/build.sh
+++ src/hello_glfw/build.sh
@@ -2,8 +2,18 @@
 
 set -e
 
+LINUX_CFLAGS="-lGL -lglfw -lGLEW"
+MACOS_CFLAGS="-I/opt/X11/include `pkg-config --cflags glew glfw3` `pkg-config --libs glew glfw3` -framework OpenGL"
+
+if [ "`uname -s`" == "Darwin" ]; then
+  CFLAGS="$MACOS_CFLAGS"
+else
+  CFLAGS="$LINUX_CFLAGS"
+fi
+
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR" #> /dev/null
 mkdir -p ./build
 pushd ./build #> /dev/null
-c++ -std=c++11 -o hello_unix.exe ../hello_unix.cpp -I/usr/local/include -g -lGL -lglfw -lGLEW
+c++ -std=c++11 -o hello_unix.exe ../hello_unix.cpp -I/usr/local/include -g $CFLAGS
