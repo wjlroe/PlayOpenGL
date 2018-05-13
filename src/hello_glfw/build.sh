397ddc338af9e8cb95cfc46a778fc1bde7d52e11
William Roe <git@wjlr.org.uk>
2018-05-13 20:03:24 +0100

    Build files for unix

--- src/hello_glfw/build.sh
+++ src/hello_glfw/build.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR" #> /dev/null
 mkdir -p ./build
 pushd ./build #> /dev/null
-c++ -std=c++11 -o hello_unix.exe ../hello_unix.cpp -I/usr/local/include -I/opt/X11/include -g `pkg-config --cflags glew glfw3` `pkg-config --libs glew glfw3`
+c++ -std=c++11 -o hello_unix.exe ../hello_unix.cpp -I/usr/local/include -g -lGL -lglfw -lGLEW
