304f4049ddb343dc01cf4babed322ca3d042604d
William Roe <git@wjlr.org.uk>
2018-05-13 20:04:38 +0100

    Move code out of win32

--- src/hello_glfw/hello_unix.cpp
+++ src/hello_glfw/hello_unix.cpp
@@ -8,9 +8,27 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "hello.cpp"
+// clang-format off
 #include "maths.cpp"
+#include "hello.cpp"
+// clang-format on
 
-int main() {
-  return run();
+char *LoadFile(const char *Filename, bool *Success) {
+    char *FileContents;
+    int FileSize = 0;
+    FILE *handle = fopen(Filename, "rb");
+
+    fseek(handle, 0L, SEEK_END);
+    FileSize = ftell(handle);
+    fseek(handle, 0L, SEEK_SET);
+    FileContents = (char *)malloc(FileSize + 1);
+    size_t size = fread(FileContents, 1, FileSize, handle);
+    FileContents[size] = 0;  // Add terminating zero.
+
+    fclose(handle);
+    *Success = true;
+
+    return FileContents;
 }
+
+int main() { return run(); }
