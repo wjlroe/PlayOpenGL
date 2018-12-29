c7a6446e1e4639a4eb0cb2326dd20e96853cd71a
William Roe <git@wjlr.org.uk>
2018-12-29 17:18:11 +0000

    Trailing whitespace

--- src/hello_glfw/hello.cpp
+++ src/hello_glfw/hello.cpp
@@ -106,20 +106,20 @@ void LogGLParams() {
 #if 0
 void ErrorExit(const char *lpszFunction) {
     // Retrieve the system error message for the last-error code
-    
+
     LPVOID lpMsgBuf;
     LPVOID lpDisplayBuf;
     DWORD dw = GetLastError();
     printf("LastError: %d\n", dw);
-    
+
     FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
                   FORMAT_MESSAGE_IGNORE_INSERTS,
                   NULL, dw, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
                   (LPTSTR)&lpMsgBuf, 0, NULL);
     printf("\nError: %s\n", (LPTSTR)lpMsgBuf);
-    
+
     // Display the error message and exit the process
-    
+
     lpDisplayBuf = (LPVOID)LocalAlloc(
         LMEM_ZEROINIT,
         (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) *
@@ -128,7 +128,7 @@ void ErrorExit(const char *lpszFunction) {
                     TEXT("%s failed with error %d: %s"), lpszFunction, dw,
                     lpMsgBuf);
     // MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
-    
+
     LocalFree(lpMsgBuf);
     LocalFree(lpDisplayBuf);
     ExitProcess(dw);
